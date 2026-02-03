8b1f6909b8d52a22622ff40ddafead2e8b991784
iamsabinakhanal <khanalsabina442@gmail.com>
2026-02-03 20:29:09 +0545

    feat: implement centralized HTTP error handling

--- src/errors/http-errors.ts
+++ src/errors/http-errors.ts
@@ -1,21 +1,7 @@
-/**
- * Custom HttpError class for Event-Hub backend
- * Can be thrown anywhere in services or controllers
- * to standardize error responses
- */
 export class HttpError extends Error {
-    statusCode: number;
-
+    statusCode: number
     constructor(statusCode: number, message: string) {
-        super(message); // Call the base Error constructor
+        super(message);
         this.statusCode = statusCode;
-
-        // Set the prototype explicitly (required for TypeScript)
-        Object.setPrototypeOf(this, HttpError.prototype);
-
-        // Optional: capture stack trace
-        if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, this.constructor);
-        }
     }
 }
